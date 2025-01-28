a7f5062463e7041da94800fb75032f35ef3c4ad3
Delta 4 <156182500+delta4chat@users.noreply.github.com>
2025-01-28 13:49:41 +0800

    chan.rs: avoid receiving message from sender that sends that message

--- src/chan.rs
+++ src/chan.rs
@@ -3,39 +3,16 @@
 use core::sync::atomic::{
     AtomicBool,
     AtomicUsize,
-    Ordering::{Relaxed, Acquire},
+    Ordering::{Relaxed, Acquire, SeqCst},
 };
 
 extern crate alloc;
 use alloc::sync::Arc;
 
+use crate::tree_index::TreeIndex;
 use crate::Queue;
 use crate::ebr::Guard;
 use crate::linked_list::Entry;
-
-#[derive(Debug)]
-struct ChanInner<T> {
-    msg: Queue<T>,
-    len: AtomicUsize,
-    size: Option<usize>,
-    closed: AtomicBool,
-}
-unsafe impl<T> Send for ChanInner<T> {}
-unsafe impl<T> Sync for ChanInner<T> {}
-
-/// (Experimental) Multi-producer, multi-consumer FIFO channel.
-#[derive(Debug)]
-pub struct Chan<T>(
-    Arc<ChanInner<T>>
-);
-unsafe impl<T> Send for Chan<T> {}
-unsafe impl<T> Sync for Chan<T> {}
-
-impl<T> Clone for Chan<T> {
-    fn clone(&self) -> Self {
-        Self(self.0.clone())
-    }
-}
 
 /// Error type for Chan
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
@@ -56,8 +33,6 @@ pub enum ChanError {
     Other(String),
 }
 
-impl core::error::Error for ChanError {}
-
 impl core::fmt::Display for ChanError {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> Result<(), core::fmt::Error> {
         use ChanError::*;
@@ -76,6 +51,79 @@ impl core::fmt::Display for ChanError {
     }
 }
 
+impl core::error::Error for ChanError {}
+
+#[derive(Debug)]
+struct ChanInner<T: 'static> {
+    queues: TreeIndex<usize, Arc<Queue<T>>>, // sender_id -> Queue<T>
+    len: AtomicUsize,
+    size: Option<usize>,
+    closed: AtomicBool,
+    chan_id_counter: AtomicUsize,
+}
+unsafe impl<T> Send for ChanInner<T> {}
+unsafe impl<T> Sync for ChanInner<T> {}
+
+impl<T> ChanInner<T> {
+    fn new(size: Option<usize>) -> Self {
+        Self {
+            queues: Default::default(),
+            len: AtomicUsize::new(0),
+            size,
+            closed: AtomicBool::new(false),
+            chan_id_counter: AtomicUsize::new(100),
+        }
+    }
+
+    fn gen_id(&self) -> usize {
+        self.chan_id_counter.fetch_update(SeqCst, SeqCst, |c| {
+            if c < usize::MAX {
+                Some(c + 1)
+            } else {
+                panic!("Chan ID has been exhausted!")
+            }
+        }).unwrap()
+    }
+}
+
+/// (Experimental) multi-producer, multi-consumer (MPMC) channel backend utilizes a HashMap and Queue.
+///
+/// Typically, this is a first-in, first-out (FIFO) channel, provided that only a single sender is active concurrently.
+///
+/// However, in the event that multiple senders are active concurrently, there is unable to guarantee the ordering of messages.
+///
+/// # in the causes of multi-senders
+/// If the `rand` feature was not enabled (by default), then the Sender with the smallest ID would be prioritized over the others (the default behavior of [`TreeIndex::iter()`]), which could starve the other Sender with the larger ID.
+///
+/// if enables the `rand` feature, then [`rand::seq::SliceRandom::shuffle`](https://docs.rs/rand/0.9.0/rand/seq/trait.SliceRandom.html#tymethod.shuffle) is used for shuffled disorderly iterating, which should ensure some degree of fairness. 
+#[derive(Debug)]
+pub struct Chan<T: 'static> {
+    id: usize,
+    dropped: bool,
+    inner: Arc<ChanInner<T>>,
+}
+unsafe impl<T> Send for Chan<T> {}
+unsafe impl<T> Sync for Chan<T> {}
+
+impl<T> Drop for Chan<T> {
+    fn drop(&mut self) {
+        if self.dropped {
+            return;
+        }
+        self.dropped = true;
+
+        self.close();
+    }
+}
+
+impl<T> Clone for Chan<T> {
+    fn clone(&self) -> Self {
+        let inner = self.inner.clone();
+        let id = inner.gen_id();
+        Self::init(id, inner)
+    }
+}
+
 impl<T> Default for Chan<T> {
     fn default() -> Self {
         Self::new(None)
@@ -83,80 +131,82 @@ impl<T> Default for Chan<T> {
 }
 
 impl<T> Chan<T> {
-    /// create a Channel with optional size (max length of queued messages)
-    pub fn new(size: Option<usize>) -> Self {
-        Self(Arc::new(ChanInner {
-            msg: Default::default(),
-            len: AtomicUsize::new(0),
-            size,
-            closed: AtomicBool::new(false),
-        }))
+    fn init(id: usize, inner: Arc<ChanInner<T>>) -> Self {
+        let _ = inner.queues.insert(id, Default::default());
+        Self {
+            id,
+            inner,
+            dropped: false,
+        }
     }
 
-    /// get the current length of channel.
+    /// create a Channel with optional size (max length of queued messages)
+    pub fn new(size: Option<usize>) -> Self {
+        let inner = Arc::new(ChanInner::new(size));
+        let id = inner.gen_id();
+        Self::init(id, inner)
+    }
+
+    /// get current number of senders. this internally calls [`TreeIndex::len()`] so the time complexity is `O(N)`.
+    pub fn senders(&self) -> usize {
+        self.inner.queues.len()
+    }
+
+    /// get the current length of queued messages in this channel.
     pub fn len(&self) -> usize {
-        self.0.len.load(Acquire)
+        self.inner.len.load(Acquire)
     }
 
     /// checks whether this channel closed.
     pub fn is_closed(&self) -> bool {
-        self.0.closed.load(Relaxed)
+        if self.dropped {
+            return true;
+        }
+
+        if self.inner.closed.load(Relaxed) {
+            return true;
+        }
+
+        ! self.inner.queues.contains(&self.id)
     }
 
-    /// closing this channel.
+    /// closing this sender and remove it from global register.
     pub fn close(&self) {
-        self.0.closed.store(true, Relaxed);
+        let _ = self.inner.queues.remove(&self.id);
     }
 
-    /// try receive message from this channel.
-    /// this is never blocking, if there is no message, it will returns ChanError::Empty.
-    pub fn try_recv(&self) -> Result<T, ChanError> {
+    /// globally closing this channel so all senders will no longer able to send messages.
+    pub fn close_all(&self) {
+        self.inner.closed.store(true, Relaxed);
+        self.inner.queues.clear();
+    }
+
+    /// send message.
+    pub fn send(&self, val: T) -> Result<(), ChanError> {
         if self.is_closed() {
             return Err(ChanError::Closed);
         }
-        if let Some(mut shared) = self.0.msg.pop() {
-            let _ = self.0.len.fetch_update(
-                Acquire,
-                Acquire,
-                |l| {
-                    if l > 0 {
-                        Some(l - 1)
-                    } else {
-                        None
-                    }
-                }
-            );
-
-            if let Some(entry) = unsafe { shared.get_mut() } {
-                Ok(unsafe { entry.take_inner() })
-            } else {
-                let guard = Guard::new();
-                let ptr = shared.get_guarded_ptr(&guard).as_ptr();
-                let entry = unsafe { &mut *(ptr as *mut Entry<T>) };
-                Ok(unsafe { entry.take_inner() })
-            }
-        } else {
-            Err(ChanError::Empty)
-        }
-    }
-
-    /// Asynchronous receive message from this channel
-    pub fn recv(&self) -> ChanRecv<T> {
-        ChanRecv(self.clone())
-    }
-
-    /// send message but does not checking the lifetime of T.
-    pub unsafe fn send_unchecked(&self, val: T) -> Result<(), ChanError> {
-        if self.is_closed() {
-            return Err(ChanError::Closed);
-        }
-        if let Some(size) = self.0.size {
+        if let Some(size) = self.inner.size {
             if self.len() >= size {
                 return Err(ChanError::Full);
             }
         }
-        unsafe { self.0.msg.push_unchecked(val); }
-        let _ = self.0.len.fetch_update(
+
+        let queue =
+            match
+                self.inner.queues.peek_with(&self.id, |k, v| {
+                    assert_eq!(self.id, *k);
+                    v.clone()
+                })
+            {
+                Some(q) => q,
+                _ => {
+                    return Err(ChanError::Closed);
+                }
+            };
+
+        queue.push(val);
+        let _ = self.inner.len.fetch_update(
             Acquire,
             Acquire,
             |l| {
@@ -169,10 +219,68 @@ impl<T> Chan<T> {
         );
         Ok(())
     }
+
+    /// try receive message from this channel.
+    /// this is never blocking, if there is no message, it will returns ChanError::Empty.
+    pub fn try_recv(&self) -> Result<T, ChanError> {
+        if self.is_closed() {
+            return Err(ChanError::Closed);
+        }
+
+        let guard = Guard::new();
+        let iter = self.inner.queues.iter(&guard);
+
+        #[cfg(feature="rand")]
+        let iter = {
+            use rand::seq::SliceRandom;
+            let mut vec: Vec<(&usize, &Arc<Queue<T>>)> = iter.collect();
+            let mut rng = rand::thread_rng();
+            vec.shuffle(&mut rng);
+            vec.into_iter()
+        };
+
+        for (id, queue) in iter {
+            if *id == self.id {
+                // skip myself.
+                // so a thread both sending and receiving will only receive messages from other senders.
+                continue;
+            }
+
+            if let Some(mut shared) = queue.pop() {
+                let _ = self.inner.len.fetch_update(
+                    Acquire,
+                    Acquire,
+                    |l| {
+                        if l > 0 {
+                            Some(l - 1)
+                        } else {
+                            None
+                        }
+                    }
+                );
+
+                if let Some(entry) = unsafe { shared.get_mut() } {
+                    return Ok(unsafe { entry.take_inner() });
+                } else {
+                    let guard = Guard::new();
+                    let ptr = shared.get_guarded_ptr(&guard).as_ptr();
+                    let entry = unsafe { &mut *(ptr as *mut Entry<T>) };
+                    return Ok(unsafe { entry.take_inner() });
+                }
+            }
+        }
+
+        Err(ChanError::Empty)
+    }
+
+    /// Asynchronous receive message from this channel
+    pub fn recv(&self) -> ChanRecv<T> {
+        ChanRecv(self.clone())
+    }
 }
 
 /// Chan::recv() returns this type that implements [`core::future::Future`] for awaiting
-pub struct ChanRecv<T>(
+pub struct ChanRecv<T: 'static>(
     Chan<T>
 );
 impl<T> core::future::Future for ChanRecv<T> {
@@ -196,9 +304,3 @@ impl<T> core::future::Future for ChanRecv<T> {
     }
 }
 
-impl<T: 'static> Chan<T> {
-    /// send message.
-    pub fn send(&self, val: T) -> Result<(), ChanError> {
-        unsafe { self.send_unchecked(val) }
-    }
-}
