caf3de25d1c91f128b1af4bc1b90853ab728e42e
Delta 4 <156182500+delta4chat@users.noreply.github.com>
2025-01-28 21:08:48 +0800

    chan.rs: inline

--- src/chan.rs
+++ src/chan.rs
@@ -15,11 +15,13 @@ use crate::ebr::Guard;
 use crate::linked_list::Entry;
 
 /// another method for create Chan instance.
+#[inline]
 pub fn new<T>(size: Option<usize>) -> Chan<T> {
     Chan::new(size)
 }
 
 /// create Chan instance, then split it to get tuple for (Sender, Receiver)
+#[inline]
 pub fn split<T>(size: Option<usize>) -> (Chan<T>, Chan<T>) {
     let chan = new(size);
     chan.split().unwrap()
@@ -28,6 +30,7 @@ pub fn split<T>(size: Option<usize>) -> (Chan<T>, Chan<T>) {
 /// provide compatibility for `smol::channel`, `async_channel` and `async_std::channel`.
 ///
 /// create (Sender, Receiver) tuple with bounded size (limited number of queued messages).
+#[inline]
 pub fn bounded<T>(size: usize) -> (Chan<T>, Chan<T>) {
     split(Some(size))
 }
@@ -35,6 +38,7 @@ pub fn bounded<T>(size: usize) -> (Chan<T>, Chan<T>) {
 /// provide compatibility for `smol::channel`, `async_channel`, and `async_std::channel`.
 ///
 /// create (Sender, Receiver) tuple with unbounded size. (unlimited number of queued messages).
+#[inline]
 pub fn unbounded<T>() -> (Chan<T>, Chan<T>) {
     split(None)
 }
@@ -46,6 +50,7 @@ pub mod std {
     /// provide compatibility for `std::sync::mpsc::unbounded` and `std::sync::mpmc::unbounded`.
     ///
     /// internally calls [`unbounded()`]
+    #[inline]
     pub fn channel<T>() -> (Chan<T>, Chan<T>) {
         unbounded()
     }
@@ -53,6 +58,7 @@ pub mod std {
     /// provide compatibility for `std::sync::mpsc::bounded` and `std::sync::mpmc::bounded`.
     ///
     /// internally calls [`unbounded()`]
+    #[inline]
     pub fn sync_channel<T>(size: usize) -> (Chan<T>, Chan<T>) {
         bounded(size)
     }
@@ -65,6 +71,7 @@ pub mod tokio {
     /// provide compatibility for `tokio::sync::mpsc::channel`.
     ///
     /// internally calls [`bounded()`]
+    #[inline]
     pub fn channel<T>(size: usize) -> (Chan<T>, Chan<T>) {
         bounded(size)
     }
@@ -72,6 +79,7 @@ pub mod tokio {
     /// provide compatibility for `tokio::sync::mpsc::unbounded_channel`.
     ///
     /// internally calls [`unbounded()`]
+    #[inline]
     pub fn unbounded_channel<T>() -> (Chan<T>, Chan<T>) {
         unbounded()
     }
@@ -103,6 +111,7 @@ pub enum ChanError {
 }
 
 impl core::fmt::Display for ChanError {
+    #[inline]
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> Result<(), core::fmt::Error> {
         use ChanError::*;
         f.write_str(
@@ -138,6 +147,7 @@ unsafe impl<T> Send for ChanInner<T> {}
 unsafe impl<T> Sync for ChanInner<T> {}
 
 impl<T> ChanInner<T> {
+    #[inline]
     fn new(size: Option<usize>) -> Self {
         Self {
             skip_queue_id: AtomicUsize::new(0), // zero is invalid value due to chan_id start from 100.
@@ -150,6 +160,7 @@ impl<T> ChanInner<T> {
         }
     }
 
+    #[inline]
     fn gen_id(&self) -> usize {
         self.chan_id_counter.fetch_update(
             SeqCst,
@@ -164,10 +175,12 @@ impl<T> ChanInner<T> {
         ).unwrap()
     }
 
+    #[inline]
     fn add_waker(&self, waker: core::task::Waker) {
         self.wakers.push(waker);
     }
 
+    #[inline]
     fn wake_one(&self) -> bool {
         if let Some(waker) = self.wakers.pop() {
             waker.wake_by_ref();
@@ -177,6 +190,7 @@ impl<T> ChanInner<T> {
         }
     }
 
+    #[inline]
     fn wake(&self, max: usize) -> usize {
         let mut wakes = 0;
         for _ in 0..max {
@@ -191,6 +205,7 @@ impl<T> ChanInner<T> {
         wakes
     }
 
+    #[inline]
     fn wake_all(&self) -> usize {
         let mut wakes: usize = 0;
         loop {
@@ -226,6 +241,7 @@ unsafe impl<T> Send for Chan<T> {}
 unsafe impl<T> Sync for Chan<T> {}
 
 impl<T> Drop for Chan<T> {
+    #[inline]
     fn drop(&mut self) {
         if self.dropped {
             return;
@@ -237,6 +253,7 @@ impl<T> Drop for Chan<T> {
 }
 
 impl<T> Clone for Chan<T> {
+    #[inline]
     fn clone(&self) -> Self {
         let flag = self.flag();
         let inner = self.inner.clone();
@@ -247,6 +264,7 @@ impl<T> Clone for Chan<T> {
 }
 
 impl<T> Default for Chan<T> {
+    #[inline]
     fn default() -> Self {
         Self::unbounded()
     }
@@ -265,6 +283,7 @@ impl<T> Chan<T> {
     /// flag value for Closed
     pub const CLOSED:    u8 = b'c';
 
+    #[inline]
     fn init(id: usize, flag: u8, inner: Arc<ChanInner<T>>) -> Self {
         let _ = inner.queues.insert(id, Default::default());
         Self {
@@ -276,6 +295,7 @@ impl<T> Chan<T> {
     }
 
     /// create a Channel with optional size (max length of queued messages)
+    #[inline]
     pub fn new(size: Option<usize>) -> Self {
         let inner = Arc::new(ChanInner::new(size));
         let id = inner.gen_id();
@@ -283,11 +303,13 @@ impl<T> Chan<T> {
     }
 
     /// create bounded Channel (limited number of queued messages).
+    #[inline]
     pub fn bounded(size: usize) -> Self {
         Self::new(Some(size))
     }
 
     /// create unbounded Channel (unlimited number of queued messages).
+    #[inline]
     pub fn unbounded() -> Self {
         Self::new(None)
     }
@@ -295,6 +317,7 @@ impl<T> Chan<T> {
     /// create two side from this channel: the left side is sender, and the right side is receiver.
     ///
     /// this is does not work if this is not a bidirectional channel.
+    #[inline]
     pub fn split(&self) -> Result<(Chan<T>, Chan<T>), ChanError> {
         let flag = self.flag();
 
@@ -315,10 +338,12 @@ impl<T> Chan<T> {
     }
 
     /// get current flag of this channel.
+    #[inline]
     pub fn flag(&self) -> u8 {
         self.flag.load(Relaxed)
     }
 
+    #[inline]
     fn set_flag(&self, flag: u8) {
         if self.is_closed() {
             return;
@@ -327,38 +352,49 @@ impl<T> Chan<T> {
     }
 
     /// restrict this Chan instance for receive message only.
+    #[inline]
     pub fn recv_only(&self) {
         self.set_flag(Self::RECV_ONLY);
     }
+
     /// restrict this Chan instance for send message only.
+    #[inline]
     pub fn send_only(&self) {
         self.set_flag(Self::SEND_ONLY);
     }
 
     /// checks whether this channel is bidirectional (can send and receive messages)
+    #[inline]
     pub fn is_bidirectional(&self) -> bool {
         self.flag() == Self::RECV_SEND
     }
+
     /// checks whether this channel able to receive messages.
+    #[inline]
     pub fn is_receiver(&self) -> bool {
         self.is_bidirectional() || self.flag() == Self::RECV_ONLY
     }
+
     /// checks whether this channel able to send messages.
+    #[inline]
     pub fn is_sender(&self) -> bool {
         self.is_bidirectional() || self.flag() == Self::SEND_ONLY
     }
 
     /// get current number of senders. this internally calls [`TreeIndex::len()`] so the time complexity is `O(N)`.
+    #[inline]
     pub fn senders(&self) -> usize {
         self.inner.queues.len()
     }
 
     /// get the current length of queued messages in this channel.
+    #[inline]
     pub fn len(&self) -> usize {
         self.inner.len.load(Acquire)
     }
 
     /// checks whether this channel closed.
+    #[inline]
     pub fn is_closed(&self) -> bool {
         if self.dropped {
             return true;
@@ -376,12 +412,14 @@ impl<T> Chan<T> {
     }
 
     /// closing this sender and remove it from global register.
+    #[inline]
     pub fn close(&self) {
         let _ = self.inner.queues.remove(&self.id);
         self.set_flag(Self::CLOSED);
     }
 
     /// globally closing this channel so all senders will no longer able to send messages.
+    #[inline]
     pub fn close_all(&self) {
         self.close();
         self.inner.global_closed.store(true, Relaxed);
@@ -389,6 +427,7 @@ impl<T> Chan<T> {
     }
 
     /// send message.
+    #[inline]
     pub fn send(&self, val: T) -> Result<(), ChanError> {
         if self.is_closed() {
             return Err(ChanError::Closed);
@@ -453,6 +492,7 @@ impl<T> Chan<T> {
         Ok(())
     }
 
+    #[inline]
     fn try_pop_from(&self, id: &usize, queue: &Arc<Queue<T>>) -> Option<T> {
         if let Some(mut shared) = queue.pop() {
             // received message from this queue, skipping it after (if multi senders exists)
@@ -485,6 +525,7 @@ impl<T> Chan<T> {
 
     /// try receive message from this channel.
     /// this is never blocking, if there is no message, it will returns ChanError::Empty.
+    #[inline]
     pub fn try_recv(&self) -> Result<T, ChanError> {
         if self.is_closed() {
             return Err(ChanError::Closed);
@@ -542,6 +583,7 @@ impl<T> Chan<T> {
         Err(ChanError::Empty)
     }
 
+    #[inline]
     fn clone_without_change_id(&self) -> Self {
         let flag = self.flag();
         let inner = self.inner.clone();
@@ -551,6 +593,7 @@ impl<T> Chan<T> {
     }
 
     /// Asynchronous receive message from this channel
+    #[inline]
     pub fn recv(&self) -> ChanRecv<T> {
         ChanRecv(self.clone_without_change_id())
     }
@@ -563,6 +606,7 @@ pub struct ChanRecv<T: 'static>(
 impl<T> core::future::Future for ChanRecv<T> {
     type Output = Result<T, ChanError>;
 
+    #[inline]
     fn poll(
         self: core::pin::Pin<&mut Self>,
         ctx: &mut core::task::Context<'_>
